35e71779604e92102c5f1dc6749b1c7ab1e7fe03
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-10-25 16:43:07 +0300

    songer

--- src/main/java/com/asac/songr/Album.java
+++ src/main/java/com/asac/songr/Album.java
@@ -1,17 +1,59 @@
 package com.asac.songr;
 
-public class Album {
+import java.io.Serializable;
+
+public class Album implements Serializable {
     private String title;
     private String artist;
-    private String songCount;
+    private int songCount;
     private int length;
     private String imageUrl;
 
-    public Album(String title, String artist, String songCount, int length, String imageUrl) {
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
         this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 }
--- src/main/java/com/asac/songr/Controller.java
+++ src/main/java/com/asac/songr/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Struct;
+import java.util.ArrayList;
 //import org.springframework.web.bind.annotation.RestController;
 
 @org.springframework.stereotype.Controller
@@ -13,7 +14,7 @@ public class Controller {
     @GetMapping("/hello")
         @ResponseBody
     public String helloworld (){
-return "hhhhhhh";
+return "helloworld";
 
     }
         @GetMapping("/capitalize/{word}")
@@ -24,11 +25,24 @@ return "hhhhhhh";
                 return  word.toUpperCase();
 
         }
-        @GetMapping("/albums")
-        @ResponseBody
+
+    @GetMapping("/albums")
+
+    public String listAlbum(Model model) {
+        Album album1 = new Album("Tammally Maak","Amr Diab",10,235,"https://upload.wikimedia.org/wikipedia/en/e/e0/Tamally-maak.jpg");
+        Album album2 = new Album("Ahla W Ahla","Amr Diab",11,321,"https://nayformedia.com/wp-content/uploads/2019/02/Ahla-W-Ahla-2016.jpg");
+        Album albums[] = {album1,album2};
+
+        model.addAttribute("albums", albums);
+        return "album.html";
+    }
+        @GetMapping("/")
+
     public String albums (){
 
-Album album1 = new Album("Tammally Maak","Amr Diab",280,"")
-    }
+return "home.html";
+
+
+        }
         }
 
