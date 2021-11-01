b16fe4b5c6f15d31a3f8339158e9631f8331ee04
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-11-01 12:57:37 +0200

    test

--- src/test/java/com/asac/songr/SongrApplicationTests.java
+++ src/test/java/com/asac/songr/SongrApplicationTests.java
@@ -2,12 +2,18 @@ package com.asac.songr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class SongrApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+@Test  void testGetSetForAlbum(){
+		Album album = new Album("t","t",1,1,"t");
+		String test = album.getTitle();
+	System.out.println(test);
+assertEquals("test getters","t",test);
 
+}
 }
